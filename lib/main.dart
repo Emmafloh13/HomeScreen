9e5aa6edbc37ae57db5ea82930ee74e639958798
Emmafloh13 <emmafh342@gmail.com>
2023-02-16 11:25:26 -0600

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 
-import 'package:contador/screens/home_screen.dart';
+import 'package:contador/screens/counter_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(CounterApp());
 }
 class CounterApp extends StatelessWidget{
+  const CounterApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Se ignoro: prefer_const_constructors
    return MaterialApp(
     // Ignore: prefer_const_constructors
   debugShowCheckedModeBanner: false,
-  home: HomeScreen(),
+  home: CounterScreen(),
    );
   }
 
